88cd1bcd3319e622c9460a17be07569aa2396c77
amitk161 <a.kamitkumar1920@gmail.com>
2021-08-19 22:07:40 +0530

    Adding all files

---
+++ app/src/main/java/com/example/crytoalgo/App.java
@@ -0,0 +1,47 @@
+package com.example.crytoalgo;
+
+import android.app.AlertDialog;
+import android.app.Application;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.Configuration;
+import android.util.Log;
+import android.widget.Toast;
+import java.util.Locale;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        Configuration newConfig = new Configuration();
+        newConfig.locale = Locale.ENGLISH;
+        super.onConfigurationChanged(newConfig);
+        Locale.setDefault(newConfig.locale);
+        getBaseContext().getResources().updateConfiguration(newConfig, getResources().getDisplayMetrics());
+
+    }
+
+    public static void Logger(String m){
+        Log.e("mip tag", m);
+    }
+
+    public static void ToastMaker(Context c, String m){
+        Toast.makeText(c, m, Toast.LENGTH_SHORT).show();
+    }
+
+    public static void DialogMaker(Context c, String title, String mes)
+    {
+        AlertDialog.Builder alert = new AlertDialog.Builder(c);
+        alert.setCancelable(true);
+        alert.setTitle(title);
+        alert.setMessage(mes);
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        alert.show();
+    }
+}
